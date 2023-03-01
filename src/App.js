a0f923c7192e792a7762f9ae1493cf94ce431759
Jonast96 <Jon.Astveit@hotmail.com>
2023-03-01 08:32:20 +0100

    inital nav/footer demo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Link, useParams, Routes, Route } from 'react-router-dom';
 import Nav from './components/header';
+import Footer from './components/footer';
 
 
 import Deals from './views/deals/deals';
@@ -9,21 +10,31 @@ import WhatsNew from './views/whatsNew/whatsNew';
 import Cart from './views/cart/cart';
 import "./styles/styles.scss"
 
+function Layout(props) {
+  return (
+    <>
+      <Nav />
+      {props.children}
+      <Footer />
+    </>
+  )
+}
+
 
 
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Routes>
-        <Route path='/deals' element={<Deals />} />
-        <Route path='/' element={<Home />} />
-        <Route path='/support' element={<Support />} />
-        <Route path='/whats-new' element={<WhatsNew />} />
-        <Route path='/cart' element={<Cart />} />
-
-      </Routes>
+    <div className='app'>
+      <Layout>
+        <Routes>
+          <Route path='/deals' element={<Deals />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/support' element={<Support />} />
+          <Route path='/whats-new' element={<WhatsNew />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,31 @@
 import { Link } from 'react-router-dom';
-
+import React from 'react';
 
 export default function Nav() {
+
+  const [dropDownMenu, setDropDownMenu] = React.useState(false)
+  function changeState () {
+    setDropDownMenu((prevState) => !prevState )
+  }
+
     return (
       <nav className='nav'>
         <h1><Link className='h1' to={"/"}>DealDash</Link></h1>
-        <ul>
+        <div onClick={()=>{changeState()}} className={dropDownMenu ? "hamburger active" : "hamburger"}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+        <div className={dropDownMenu ? "dropdown active": "dropdown"}>
+        <p className='flex-item'><Link to={"/cart"}>Cart</Link></p>
+        <ul className='flex-item'>
           <li><Link to={"/deals"}>Deals</Link></li>
           <li><Link to={"/whats-new"}>What's New</Link></li>
           <li><Link to={"/support"}>Support</Link></li>
         </ul>
-        <input type={'search'}></input>
-        <p><Link to={"/cart"}>Cart</Link></p>
+        <input className='flex-item' placeholder='Search items' type={'search'}></input>
+        </div>
       </nav>
     )
   }
